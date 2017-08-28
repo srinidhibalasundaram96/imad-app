bedaa69c52039b29a936623d78f1c38513df897a
srinidhibalasundaram96 <30665144+srinidhibalasundaram96@users.noreply.github.com>
2017-08-28 18:23:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
  app.get("/articleOne", function(req,res) {
-     res.send("there's no such file you idiot go and create one");
+  res.sendFile(path.join(__dirname, 'ui', 'articleOne.html'));
  });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
